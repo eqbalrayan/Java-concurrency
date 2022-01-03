b1b505049132caf8504978a8b618ea2720b0a816
eqbalrayan <70439641+eqbalrayan@users.noreply.github.com>
2022-01-03 09:50:04 +0530

    Add files via upload

--- JavaThread.java
+++ JavaThread.java
@@ -1,21 +1,32 @@
 public class JavaThread{
 
 
+    synchronized static void coutNumber(){
+      int sum=0;
+      
+        for(int i=0;i<=10;i++){
+            sum=sum+i;
+            System.out.println("My Thread ,This is my current thread   executiny"+Thread.currentThread() +"  Value of couter "+i);
+           try{Thread.sleep(2000);}catch(Exception e){} 
+           System.out.println(sum);
+        }
+    
+        System.out.println(sum);
+    }
+
+
 
 
      public static void main(String[] args) {
 
-        Runnable runnable=()->{
+        
 
-            for(int i=0;i<=10;i++){
-
-                System.out.println("Welcome  " +i);
-            }
-
-        };
-        Thread th1=new Thread(runnable);
+        Runnable runnable=JavaThread::coutNumber;
+        Thread th1=new Thread(runnable,"test 1 thread");
+        Thread th2=new Thread(runnable,"test 2 thread");
 
         th1.start();
+        th2.start();
         
     }
 }
